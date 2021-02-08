1d9f049c5755ed487aa07dc6d6868731227085eb
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-02-08 10:03:00 -0300

    Created component Navbar

---
+++ src/core/components/Navbar/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './styles.css';
+
+const Navbar = () => (
+  <nav className="row main-nav">
+    <div className="nav-logo">
+      <a href="/">
+        <h4>Bootcamp DevSuperior</h4>
+      </a>
+    </div>
+  </nav>
+);
+
+export default Navbar;
