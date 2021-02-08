74aaf959cd46bc12d6fa292c8e22b7db20288a94
José Carlos Duque Filho <cj_duque@hotmail.com>
2021-02-08 18:08:29 -0300

    API github data

---
+++ src/core/types/User.ts
@@ -0,0 +1,11 @@
+export type User = {
+  name: string;
+  blog: string;
+  location: string;
+  avatar_url: string;
+  html_url: string;
+  public_repos: number;
+  followers: number;
+  following: number;
+  created_at: string;
+}
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,42 +1,57 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './styles.css';
+import makeRequest from '../../core/utils/request';
+import { User } from '../../core/types/User';
+
 
 const Search = () => {
+
+  const [userResponse, setUserResponce] = useState<User>();
+  
+  useEffect(() => {
+    makeRequest({url: '/jose-duque'})
+    .then(response => setUserResponce(response.data))
+  }, [])
+
   return (
-    <div className="search-container">
-      <div className="search-content-container">
-        <h1>
-          Encontre um perfil Github
-        </h1>
-        <form className="search-form">
-          <input type="text" placeholder="Usuário Github"/>
-          <div className="search-btn-form">
-            <button type="submit">Encontrar</button>
+    <>
+      <div className="search-container">
+        <div className="search-content-container">
+          <h1>
+            Encontre um perfil Github
+          </h1>
+          <form className="search-form">
+            <input type="text" placeholder="Usuário Github"/>
+            <div className="search-btn-form">
+              <button type="submit">Encontrar</button>
+            </div>
+          </form>
+        </div>
+            <div className="search-perfil">
+            <div className="search-img">
+              <img src={userResponse?.avatar_url} alt="avatar" className="user-img"/>
+            </div>
+            <div className="search-data-user">
+              <div className="search-form-repository">
+                <div>Repositórios públicos: {userResponse?.public_repos}</div>
+                <div>Seguidores: {userResponse?.followers}</div>
+                <div>Seguindo: {userResponse?.following}</div>
+              </div>
+              <h3>Informações</h3>
+              <div className="search-form-user">
+                <div>Empresa: {userResponse?.name}</div><br/>
+                <div>Website/Blog:{userResponse?.blog}</div><br/>
+                <div>Localidade: {userResponse?.location}</div><br/>
+                <div>Membro desde: </div>
+              </div>
+            </div>
           </div>
-        </form>
+        
+      
       </div>
-      <div className="search-perfil">
-          <div className="search-img">
-            <h3>imagem</h3>
-          </div>
-         
-          <div className="search-data-user">
-            <form action="" className="search-form-repository">
-              <input type="text" placeholder="Repositórios públicos:"/>
-              <input type="text" placeholder="Seguidores: "/>
-              <input type="text" placeholder="Seguindo:"/>
-            </form>
-            <h3>Informações</h3>
-            <form action="" className="search-form-user">
-              <input type="text" placeholder="Empresa:"/><br/>
-              <input type="text" placeholder="Website/Blog:"/><br/>
-              <input type="text" placeholder="Localidade:"/><br/>
-              <input type="text" placeholder="Membro desde:"/>
-            </form>
-          </div>
-      </div>
-    </div>
+    </>
   );
 }
 
 export default Search;
+         
